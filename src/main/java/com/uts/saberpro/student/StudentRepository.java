2ea1d347429996c940342f781ef38a9dfd3e8d49
davidssolano <davidsantiagosolano@gmail.com>
2025-11-26 09:10:36 -0500

    Configure Student CRUD and DB env variables

---
+++ src/main/java/com/uts/saberpro/student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.uts.saberpro.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByCode(String code);
+}
