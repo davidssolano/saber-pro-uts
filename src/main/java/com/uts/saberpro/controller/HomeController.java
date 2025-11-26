1e9877fa6edd81e89dfb9459b5e7b76ada602b4e
davidssolano <davidsantiagosolano@gmail.com>
2025-11-26 12:46:02 -0500

    Agregar controlador Home y vista index

---
+++ src/main/java/com/uts/saberpro/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.uts.saberpro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // va a buscar templates/index.html
+    }
+}
